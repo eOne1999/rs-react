b12912eac2b94608ffa72d3c4d4097bda30528e0
Ivan Dmitriev <dmitriev_vanya@mail.ru>
2023-03-21 13:58:16 +0300

    feat: set vitest and react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,24 @@
 // import { Routes, Route, Link } from 'react-router-dom';
 
-function App() {
-  // const [count, setCount] = useState(0);
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
+export function App() {
   return (
-    <div className="App">
-      <div>
-        Hello World!
-        <div className="feefef" />
-      </div>
-      {/* //   <div>
-    //     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is
-    //       {' '}
-    //       {count}
-    //     </button>
-    //     <p>
-    //       Edit
-    //       {' '}
-    //       <code>src/App.tsx</code>
-    //       {' '}
-    //       and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p> */}
-    </div>
+    // <div className="App">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    // </div>
   );
 }
 
-export default App;
+export function WrappedApp() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
