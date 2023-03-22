7f952a32df9d94d5e825aaf61789134b3ea83745
Ivan Dmitriev <dmitriev_vanya@mail.ru>
2023-03-22 09:04:53 +0300

    feat: save and showing searchBar value

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,28 @@
-function SearchBar() {
-  return <input type="text" className="search-bar" />;
+import React from 'react';
+
+type SearchState = { searchValue: string };
+
+class SearchBar extends React.Component<object, SearchState> {
+  constructor(params: SearchState) {
+    super(params);
+    this.state = { searchValue: localStorage.getItem('searchValue') || '' };
+  }
+
+  render() {
+    const currState = this.state;
+    return (
+      <input
+        onChange={(event) => {
+          this.setState({ searchValue: event.target.value });
+          localStorage.setItem('searchValue', event.target.value.toString());
+        }}
+        type="text"
+        className="search-bar"
+        placeholder="Search"
+        value={currState.searchValue}
+      />
+    );
+  }
 }
 
 export default SearchBar;
