c086adbfb1a317d0fdef1add38997ea05fbee334
Ivan Dmitriev <dmitriev_vanya@mail.ru>
2023-03-29 19:24:04 +0300

    fix: delete comments

--- src/pages/Forms.tsx
+++ src/pages/Forms.tsx
@@ -1,8 +1,5 @@
-/* eslint-disable react/destructuring-assignment */
 import React from 'react';
-// import Card from '../components/Card';
 import Form from '../components/Form';
-// import { TFormData } from '../types';
 
 class Forms extends React.Component {
   constructor(props: object) {
@@ -10,29 +7,12 @@ class Forms extends React.Component {
     this.state = '';
   }
 
-  // eslint-disable-next-line class-methods-use-this, react/no-unused-class-component-methods
-  // addCard() {
-  //   console.log('add');
-  // }
-
   render(): React.ReactNode {
-    // const currState = this.state;
     return (
       <>
         <h1>Forms</h1>
         <p>Upload your photo!</p>
         <Form />
-        {/* <div className="cards">
-          <p>{this.state.author}</p>
-          {currState.cards.map((card) => (
-            <Card
-              key={card.id.toString()}
-              author={card.author}
-              post_url={card.post_url}
-              id={card.id}
-            />
-          ))} 
-        </div> */}
       </>
     );
   }
