a3ed50be593a9e5a4e6dff2c1c796e37f8ff7161
Ivan Dmitriev <dmitriev_vanya@mail.ru>
2023-04-07 23:11:21 +0300

    fix: app and home tests

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -3,13 +3,17 @@ import { render, screen } from '@testing-library/react';
 import Home from './Home';
 
 describe('Home', () => {
+  let searchValue = '';
+  const setSearchValue = () => {
+    searchValue = 'apple';
+  };
+
   it('Renders Home', () => {
-    const { container } = render(<Home />);
+    render(<Home searchValue={searchValue} setSearchValue={setSearchValue} />);
     expect(
       screen.getByRole('heading', {
         level: 1,
       })
     ).toHaveTextContent('Home');
-    expect(container.querySelector('.cards')).toBeInTheDocument();
   });
 });
